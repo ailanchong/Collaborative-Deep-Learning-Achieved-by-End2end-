2ad78c143daece3535c1dc2eea4fa829626d0cde
ailanchong <13241818007@163.com>
2017-11-01 23:31:24 +0800

    准备数据完毕

--- prepare_data.py
+++ prepare_data.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*- 
 import numpy as np  
 import pandas as pd 
+import os
 
 
-#order_products_prior_df = pd.read_csv("./order_products__prior.csv")
-orders_df = pd.read_csv("./orders.csv")
-print(order_products__prior_df.head())
+order_products_prior_df = pd.read_csv("./order_products__prior.csv", usecols=['order_id','product_id'])
+orders_df = pd.read_csv("./orders.csv", usecols=['order_id', 'user_id'])
+print(order_products_prior_df.head())
 
 user_products = order_products_prior_df.merge(orders_df, how='left', on='order_id')
 
