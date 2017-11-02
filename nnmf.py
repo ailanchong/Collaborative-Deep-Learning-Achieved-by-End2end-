6ed0ffed8c770bb9997b10d39d321bf94de52799
chenpengda <13241818007@163.com>
2017-11-02 18:29:53 +0800

    完成矩阵分解

--- nnmf.py
+++ nnmf.py
@@ -1,10 +1,8 @@
 import numpy as np 
 import tensorflow as tf 
-import pandas as import pd
-
-
-
-
+import pandas as  pd
+import os
+from sklearn.cross_validation import train_test_split 
 
 def generate_batch(I, J, V_IJ, batch_size):
     n_examples = I.shape[0]
@@ -22,55 +20,36 @@ def generate_batch(I, J, V_IJ, batch_size):
 
         yield batch_I, batch_J, batch_V_IJ # 生成每一个batch
 
-
-def load_data(path):
-
-
-    return I, J, V_IJ
-
-
-
-class DataReader(object):
-
-    def __init__(self, data_dir):
-        data_cols = ['i', 'j', 'V_ij']
-        data = [np.load(os.path.join(data_dir, '{}.npy'.format(i)), mmap_mode='r') for i in data_cols]
-
-        df = DataFrame(columns=data_cols, data=data)
-        self.train_df, self.val_df = df.train_test_split(train_size=0.9)
-
-        print 'train size', len(self.train_df)
-        print 'val size', len(self.val_df)
-
-        self.num_users = df['i'].max() + 1
-        self.num_products = df['j'].max() + 1
-
-    def train_batch_generator(self, batch_size):
-        return self.batch_generator(
-            batch_size=batch_size,
-            df=self.train_df,
-            shuffle=True,
-            num_epochs=10000
-        )
-
-    def val_batch_generator(self, batch_size):
-        return self.batch_generator(
-            batch_size=batch_size,
-            df=self.val_df,
-            shuffle=True,
-            num_epochs=10000
-        )
-
-    def batch_generator(self, batch_size, df, shuffle=True, num_epochs=10000, is_test=False):
-return df.batch_generator(batch_size, shuffle=shuffle, num_epochs=num_epochs, allow_smaller_final_batch=is_test)
+def load_data(data_dir):
+    data_cols = ['i', 'j', 'V_ij']
+    data = np.array([np.load(os.path.join(data_dir, '{}.npy'.format(i)), mmap_mode='r') for i in data_cols])
+    data = data.T
+    df = pd.DataFrame(data,columns=data_cols)
+    data_cols = ['i', 'j']
+    target_cols = ['V_ij']
+    data_df = df[data_cols]
+    target_df = df[target_cols]
+    train_data, test_data, train_target, test_target = train_test_split(data_df,  
+                                                   target_df,  
+                                                   test_size = 0.2,  
+                                                   random_state = 0) 
+    num_usr = df['i'].max()
+    num_pro = df['j'].max() 
 
 
+    print(data_df.head())
+    print(target_df.head())
+    print(df['i'].max())
+    print(df['j'].max())
+    print(df[df['i'] == 1])
+    print(df.shape)
+
+    return train_data, test_data, train_target, test_target, num_usr, num_pro
 
 
-
-def get_nnmf(num_usr, num_pro, rank):
-    usr_matrix = tf.Variable(tf.truncated_normal(num_usr, rank))
-    pro_matrix = tf.Variable(tf.truncated_normal(num_pro, rank))
+def get_nnmf(num_usr, num_pro, rank, i, j, V_ij):
+    usr_matrix = tf.Variable(tf.truncated_normal([num_usr, rank]))
+    pro_matrix = tf.Variable(tf.truncated_normal([num_pro, rank]))
     usr_bias = tf.Variable(tf.truncated_normal([num_usr]))
     pro_bias = tf.Variable(tf.truncated_normal([num_pro]))
     global_mean = tf.Variable(0.0)
@@ -78,88 +57,60 @@ def get_nnmf(num_usr, num_pro, rank):
     pro_j = tf.gather(pro_matrix, j)
     usr_bias = tf.gather(usr_bias, i)
     pro_bias = tf.gather(pro_bias, j)
-    interaction = tf.reduce_sum(tf.multiply(usr_i * pro_j), 1)
+    interaction = tf.reduce_sum(tf.multiply(usr_i , pro_j), 1)
     preds = global_mean + usr_bias + pro_bias + interaction
     return preds
 
-def train_nnmf(num_usr, num_pro, rank):
-    output = get_nnmf(num_usr, num_pro, rank)
-    rmse = tf.sqrt(tf.reduce_sum(tf.squared_difference(output, V_ij)))
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        step = 0
-        while True:
-
-            for batch_xs,batch_ys in generatebatch(X,Y,Y.shape[0],batch_size): # 每个周期进行MBGD算法
-
-                sess.run(train_step,feed_dict={tf_X:batch_xs,tf_Y:batch_ys})
-
-
-            batch_x, batch_y = get_next_batch(64)
-            _, loss_ = sess.run([optimizer, loss], feed_dict={X: batch_x, Y: batch_y, keep_prob: 1})
-            print(step, loss_)
-
-            # 每100 step计算一次准确率
-            if step % 100 == 0:
-                batch_x_test, batch_y_test = get_next_batch(100)
-                acc = sess.run(accuracy, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.})
-                print(step, acc)
-                # 如果准确率大于50%,保存模型,完成训练
-                if acc > 0.5:
-                    saver.save(sess, "crack_capcha.model", global_step=step)
-                    break
-            step += 1
-
-def main():
-    I, J, V_IJ = load_data(path)   #path暂时空着
-    df = load_data(path)
-    num_usr = np.max(I) + 1
-    num_pro = np.max(J) + 1
-    df_train, df_test, labels_train, labels_test = train_test_split(df, labels, train_size=0.8, test_size=0.2, random_state=42)   
-    i = tf.placeholder(dtype=tf.int32, shape=[None])
-    j = tf.placeholder(dtype=tf.int32, shape=[None])
-    V_ij = tf.placeholder(dtype=tf.float32, shape=[None])
-    num_usr = np.max(I) + 1
-    num_pro = np.max(J) + 1
+def main(path):
+    train_data, test_data, train_target, test_target, num_usr, num_pro = load_data(path)
+    train_I = np.array(train_data['i'])
+    train_J = np.array(train_data['j'])
+    train_VIJ = np.array(train_target)
+    test_I = np.array(test_data['i'])
+    test_J = np.array(test_data['j'])
+    test_VIJ = np.array(test_target)  
+    test_I = np.reshape(test_I, (-1,1))
+    test_J = np.reshape(test_J, (-1,1))
+    test_VIJ = np.reshape(test_VIJ, (-1,1))    
+    i = tf.placeholder(dtype=tf.int32, shape=[None,1])
+    j = tf.placeholder(dtype=tf.int32, shape=[None,1])
+    V_ij = tf.placeholder(dtype=tf.float32, shape=[None,1])
     rank = 25
     batch_size = 4096
-    output = get_nnmf(num_usr, num_pro, rank)
+    output = get_nnmf(num_usr+1, num_pro+1, rank, i, j, V_ij)
     rmse = tf.sqrt(tf.reduce_sum(tf.squared_difference(output, V_ij)))
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(rmse)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.005).minimize(rmse)
     saver = tf.train.Saver()
 
-    for i in range(200):
-        sess.run(optimizer)
-        if ((i + 1) % 1 == 0):
-            print("step:", i + 1, "accuracy:", sess.run(loss))
-
-    print("accuracy: " , sess.run(loss))
-    save_path = saver.save(sess, ckpt_path)
-    print("Model saved in file: %s" % save_path)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         step = 0
         while True:
-            for batch_I, batch_J, batch_V_IJ in generate_batch(I, J, V_IJ, batch_size): 
-                _, rmse_eval = sess.run([optimizer, rmse],feed_dict={i:batch_I, j:batch_J, V_ij:batch_V_IJ})
-                print ("step: %s, rmse: %s" %(step, rmse))
+            for batch_I, batch_J, batch_VIJ in generate_batch(train_I, train_J, train_VIJ, batch_size): 
+                batch_I = np.reshape(batch_I, (-1,1))
+                batch_J = np.reshape(batch_J, (-1,1))
+                batch_VIJ = np.reshape(batch_VIJ, (-1,1))
+                _, rmse_eval = sess.run([optimizer, rmse],feed_dict={i:batch_I, j:batch_J, V_ij:batch_VIJ})
+                print ("step: %s, rmse: %s" %(step, rmse_eval))
             # 每100 step计算一次准确率
-            '''
+            
             if step % 100 == 0:
-                batch_x_test, batch_y_test = get_next_batch(100)
-                acc = sess.run(accuracy, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.})
-                print(step, acc)
+                rmse_eval = sess.run(rmse, feed_dict={i:test_I, j:test_J, V_ij:test_VIJ})
+                print ("testing!!!!!!!!! step: %s, rmse: %s" %(step, rmse_eval))
                 # 如果准确率大于50%,保存模型,完成训练
-                if acc > 0.5:
-                    saver.save(sess, "crack_capcha.model", global_step=step)
+                if rmse_eval < 100:
+                    saver.save(sess, "./model/nnmf.model")
                     break
-            '''
+            
             step += 1
-            if (step % 200 == 0):
-                break
+
+
+def test():
+    load_data("./data")
+
+if __name__ == "__main__":
+    main("./data")
 
 
 
